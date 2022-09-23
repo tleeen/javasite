22d0a2cbaa2e66dfb99e2303b5b3fe5cc19ee64d
tleeen <73270927+tleeen@users.noreply.github.com>
2022-09-23 08:57:24 +0700

    Delete demo directory

--- demo/src/main/java/controller/DB/Configs.java
+++
@@ -1,9 +0,0 @@
-package controller.DB;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "1234";
-    protected String dbPass = "user";
-    protected String dbName ="javapr";
-}
